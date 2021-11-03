090216c58bcb69643a10caedd1ff772c9caa9a1b
MRColor <mattiar11@gmail.com>
2021-11-03 18:02:44 +0100

    added TxTtoJSON added new dataset in input added first web index page of 3D graph

---
+++ Dataset/TXTtoJSONson.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+
+let data = fs.readFileSync('./input/math-graph.txt', 'utf-8');
+const lines = data.split(/\r?\n/);
+let graph = {
+    'nodes': [],
+    'links': []
+}
+let archi = false
+lines.forEach((row) => {
+    if (row == '2277')
+        return
+    if (row == '2554') 
+        archi = true
+    if (!archi) {
+        let [id, discendenti, ...nome] = row.split(' ')
+        nome.join(' ');
+        const nodo = {
+            'id': id,
+            'discendenti': discendenti,
+            'name': nome.join(' ')
+
+        }
+        graph.nodes.push(nodo)
+    }else {
+        let [source, target, year, country] = row.split(' ')
+
+        const arco = {
+            'source': source,
+            'target': target,
+            'year': year,
+            'country': country
+        }
+        graph.links.push(arco)
+    }
+})
+
+//console.log(graph)
+
+let jsonContent = JSON.stringify(graph);
+
+fs.writeFileSync('./Output/graph.json', jsonContent,'utf-8');
